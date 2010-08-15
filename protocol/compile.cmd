ea9c2a2182f09b15aec3350c5c4d00630a9ed6c9
dlevshunov <dlevshunov@uosipa.com>
2010-08-15 15:54:40 +0400

    Protocol code generation script was improved.

--- protocol/compile.cmd
+++ protocol/compile.cmd
@@ -1,38 +1,14 @@
-if exist target\java rd /S /Q target\java
-if exist target\java pause 0
+call wipe.cmd
 
 mkdir target\java
-if not exist target\java pause 0
-
-pushd src
-
-protoc --java_out=..\target\java Lobby.proto
-if errorlevel 1 pause 0
-
-protoc --java_out=..\target\java Room.proto
-if errorlevel 1 pause 0
-
-protoc --java_out=..\target\java Interop.proto
-if errorlevel 1 pause 0
-
-popd
-
-
-if exist target\cpp rd /S /Q target\cpp
-if exist target\cpp pause 0
-
 mkdir target\cpp
+
+if not exist target\java pause 0
 if not exist target\cpp pause 0
 
 pushd src
-
-protoc --cpp_out=..\target\cpp Lobby.proto
-if errorlevel 1 pause 0
-
-protoc --cpp_out=..\target\cpp Room.proto
-if errorlevel 1 pause 0
-
-protoc --cpp_out=..\target\cpp Interop.proto
-if errorlevel 1 pause 0
-
+for %%i in (*.proto) do (
+    protoc --java_out=..\target\java --cpp_out=..\target\cpp %%i
+    if errorlevel 1 pause 0
+)
 popd
